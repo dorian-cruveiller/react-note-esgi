54edbacb453d835acfd2a07787df75b882fee625
Arnaud Renaud <arnaud.renaud@gmail.com>
2024-03-08 17:20:11 +0100

    Fetch and display notes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [notes, setNotes] = useState(null);
+
+  const fetchNotes = async () => {
+    const response = await fetch("/notes");
+    const data = await response.json();
+
+    setNotes(data);
+  };
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {notes?.map((note) => (
+          <div>{note.title}</div>
+        ))}
       </header>
     </div>
   );
