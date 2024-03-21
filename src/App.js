32ea3fc981b287ee85c2bf0c4641c0ad25f9626d
Arnaud Renaud <arnaud.renaud@gmail.com>
2024-03-21 15:00:14 +0100

    Show loading status while notes are loading

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
 
+// Cycle de vie du composant App :
+// Initialement : `notes` vaut `null`, donc pas d'affichage dans le header
+// Après le rendu initial : lancement de la requête au serveur (GET /notes)
+// À la réponse du serveur : `notes` devient la réponse du serveur, rafraîchissement de l'affichage
+
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
   const [notes, setNotes] = useState(null);
 
   const fetchNotes = async () => {
@@ -10,6 +16,7 @@ function App() {
     const data = await response.json();
 
     setNotes(data);
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -19,9 +26,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {notes?.map((note) => (
-          <div>{note.title}</div>
-        ))}
+        {isLoading
+          ? "Chargement…"
+          : notes?.map((note) => <div>{note.title}</div>)}
       </header>
     </div>
   );
